25633cdb46c1bf118cfeac0ef9163464ea850377
Jason Chin Yu Soo <jsoo@tyro.com>
2018-12-28 00:10:29 +1100

    Display mock form

--- public/films-client.js
+++ public/films-client.js
@@ -36,13 +36,48 @@ const displayFilmTable = (filmCols, films) => {
   document.body.appendChild(table);
 }
 
+function submitClick(event) {
+  // console.log("films: ");
+  // console.log(films);
+
+  let year = event.target.elements["Year"].value;
+  let title = event.target.elements["Title"].value;
+  let imdbRating = event.target.elements["Imdb Rating"].value;
+  let director = event.target.elements["Director"].value;
+
+  console.log("year: %s, title: %s, imdbRating: %s, director: %s", year, title, imdbRating, director);
+  // films.push({year: year, title: title, imdbRating: imdbRating, director: director});
+  // console.log(films);
+  alert("Submit clicked");
+}
+
+const displayForm = (filmCols) => {
+  document.body.appendChild(elt("h2", null, "Submit a film"));
+
+  let form = elt("form", {onsubmit: submitClick});
+  filmCols.forEach(elem => {
+    form.appendChild(elt("label", null, elem + ":"));
+    form.appendChild(elt("br", null));
+    let inputText = elt("input", {type: "text", name: elem});
+    form.appendChild(inputText);
+    form.appendChild(elt("br", null));
+  });
+
+  let submitButton = elt("button", {type: "submit"}, "Submit");
+  form.appendChild(submitButton);
+
+  document.body.appendChild(form);
+}
+
 const runApp = () => {
   let films = [
     {"year":2002,"title":"Spiderman","imdbRating":7.3,"director":"Sam Raimi"},
     {"year":2004,"title":"Spiderman 2","imdbRating":7.3,"director":"Sam Raimi"}
   ];
   let filmCols = ["Year", "Title", "Imdb Rating", "Director"];
+
   displayFilmTable(filmCols, films);
+  displayForm(filmCols);
 }
 
 // Main body
